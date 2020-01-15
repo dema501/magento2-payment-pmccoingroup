a6ed37041199efc9212cb175f77b540c3c0a9490
Dmitry Bashlov <dmitry.bashlov@theagilehub.net>
2020-01-14 22:42:00 -0600

    Bug fix, implement payment logic

--- Model/PMCCoinGroup.php
+++ Model/PMCCoinGroup.php
@@ -13,38 +13,42 @@ class PMCCoinGroup extends \Magento\Payment\Model\Method\Cc
     protected $_canRefund = false;
     protected $_canVoid = false;
 
+    private   $_curl;
+    private   $_encryptor;
+
+    /**
+     * @param \Ecomail\Ecomail\Helper\Data             $helper
+     * @param \Magento\Customer\Model\CustomerRegistry $customerRegistry
+     */
+    public function __construct(
+        \Magento\Framework\HTTP\Client\Curl $curl,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor
+    ) {
+        parent::__construct();
+ 
+        $this->_scopeConfig      = $scopeConfig;
+        $this->_curl             = $curl;
+        $this->_encryptor        = $encryptor;
+    }
+
     /**
      * Authorize a payment.
      *
      * @param \Magento\Payment\Model\InfoInterface $payment
      * @param float $amount
      * @return $this
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
-        try {
+        $response = $this->_doSale($payment);
 
-            ///build array of payment data for API request.
-            $request = [
-                'cc_type' => $payment->getCcType(),
-                'cc_exp_month' => $payment->getCcExpMonth(),
-                'cc_exp_year' => $payment->getCcExpYear(),
-                'cc_number' => $payment->getCcNumberEnc(),
-                'amount' => $amount
-            ];
-
-            //check if payment has been authorized
-            $response = $this->makeAuthRequest($request);
-
-        } catch (\Exception $e) {
-            $this->debug($payment->getData(), $e->getMessage());
-        }
-
-        if(isset($response['transactionID'])) {
-            // Successful auth request.
+        if(isset($response['id'])) {            // Successful auth request.
             // Set the transaction id on the payment so the capture request knows auth has happened.
-            $payment->setTransactionId($response['transactionID']);
-            $payment->setParentTransactionId($response['transactionID']);
+            $payment->setTransactionId($response['id']);
+            $payment->setParentTransactionId($response['id']);
         }
 
         //processing is not done yet.
@@ -60,42 +64,96 @@ class PMCCoinGroup extends \Magento\Payment\Model\Method\Cc
      */
     public function getConfigPaymentAction()
     {
-        return self::ACTION_AUTHORIZE_CAPTURE;
+        return self::ACTION_AUTHORIZE;
     }
 
-    /**
-     * Test method to handle an API call for authorization request.
-     *
-     * @param $request
-     * @return array
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function makeAuthRequest($request)
-    {
-        $response = ['transactionId' => 123]; //todo implement API call for auth request.
 
-        if(!$response) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Failed auth request.'));
+    /**
+     * Set the payment action to authorize_and_capture
+     *
+     * @return string
+     */
+    private function _doSale(\Magento\Payment\Model\InfoInterface $payment)
+    {
+        $order = $payment->getOrder();
+        $billing = $order->getBillingAddress();
+
+        $data = array(
+            "amount" => (int) $payment->getAmount() * 100, // Yes Decimal Total cents amount with up to 2 decimal places.,
+            "wallet_id" => $this->_scopeConfig->getValue('payment/pmccoingroup/wallet_id'),
+            "order_id"=> $order->getIncrementId(),
+        );
+
+        $this->_curl->setOption(CURLOPT_HTTPHEADER, $this->_getExtraHeaders());
+
+        $this->_curl->get($this->getURL() . '/v1/customers', array(
+            'page' => 1,
+            'perPage' => 3,
+            'search' => strval($order->getCustomerEmail())
+        ));
+
+        $searchCustomerResData = json_decode($this->_curl->getBody(), TRUE);
+
+        if (sizeof($searchCustomerResData["data"]) > 0 && $searchCustomerResData["data"][0]["email"] ===  strval($order->getCustomerEmail())) {
+            $data["customer"] = array(
+                "id" => $searchCustomerResData["data"][0]["id"]
+            );
+        } else {
+            $state =  substr(strval($billing->getRegionCode()), 0, 3);
+            if (empty($state)) {
+                $state = "UNW";
+            }
+
+            $data["customer"] = array(
+                "name"=> strval($billing->getFirstname()) . ' ' . strval($billing->getLastname()), // Yes String Account holder's first and last name
+                "email"  => strval($order->getCustomerEmail()), // Yes String Customer's email address. Must be a valid address. Upon processing of the draft an email will be sent to this address.
+                "phone" => strval($billing->getTelephone()),
+                "address" => array(
+                    "line1" => strval($billing->getStreet(1)),
+                    "line2" => strval($billing->getStreet(2)),
+                    "country" => strval($billing->getCountry()),
+                    "state" => $state,// Yes String The state portion of the mailing address associated with the customer's checking account. It must be a valid US state or territory
+                    "city" => strval($billing->getCity()), // Yes String The city portion of the mailing address associated with the customer's checking
+                    "zipcode" => substr(strval($billing->getPostcode()), 0, 5), // Yes String The zip code portion of the mailing address associated with the customer's checking account. Accepted formats: XXXXX,
+                ),
+                "card" => array(
+                    "cardholder_name"=> strval($billing->getFirstname()) . ' ' . strval($billing->getLastname()), // Yes String Account holder's first and last name
+                    "number" => $payment->getCcNumber(),
+                    "cvc" => $payment->getCcCid(),
+                    "expiration" => sprintf('%02d-%02d', $payment->getCcExpMonth(),  substr($payment->getCcExpYear(), -2)),
+                    "default" => true,
+                    "register" => true
+                )
+            );
         }
 
-        return $response;
+        $this->_curl->post($this->getURL() . '/v1/charges', $data);
+
+        return $this->_doValidate(json_decode($this->_curl->getBody(), TRUE), json_encode($data));
     }
 
-    /**
-     * Test method to handle an API call for capture request.
-     *
-     * @param $request
-     * @return array
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function makeCaptureRequest($request)
+    private function _doValidate($data, $postData)
     {
-        $response = ['success']; //todo implement API call for capture request.
+        if ((int) substr($data["code"], 0, 1) !== 2) {
+            $message = $data['message'];
+            foreach ($data['errors'] as $field => $error) {
+                $message .= sprintf("\r\nthe issue is in %s field - %s\r\n", $field, array_shift($error));
+            }
 
-        if(!$response) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Failed capture request.'));
+            $this->debug(array('_doValidate--->', $code, $message, $data, $postData));
+            throw new \Magento\Framework\Exception\LocalizedException(__("Error during process payment: response code: %s %s", $code, $message));
         }
 
-        return $response;
+        return $data;
+    }
+
+    private function _getExtraHeaders() {
+        return array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Cache-Control: no-cache',
+            'Authorization: ' .  $this->_encryptor->decrypt($this->_scopeConfig->getValue('payment/pmccoingroup/token')),
+            'team-id: ' .  $this->_scopeConfig->getValue('payment/pmccoingroup/team_id'),
+        );
     }
 }
